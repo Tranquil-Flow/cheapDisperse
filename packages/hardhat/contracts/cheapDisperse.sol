de9d10420a68048126bf128ed5ebe534b7143818
Tranquil-Flow <66773372+Tranquil-Flow@users.noreply.github.com>
2024-03-09 00:01:46 +1000

    add safeguards for sending eth to smart contracts

--- packages/hardhat/contracts/cheapDisperse.sol
+++ packages/hardhat/contracts/cheapDisperse.sol
@@ -11,17 +11,28 @@ contract CheapDisperse {
     error ArrayLengthOverMaxLimit();
 
     /// @notice Sends ether to an array of users
+    /// @dev Skips transfers to contracts with excessive fallback functions without reverting
     /// @param recipients A list of addresses that will receive ether
     /// @param values A list of the number of wei that each address will receive
-    function disperseEther(address[] calldata recipients, uint[] calldata values) external payable {
+    function disperseEther(address[] calldata recipients, uint[] calldata values) external payable returns (address[] memory) {
         uint length = recipients.length;
         if (length != values.length) {revert ArrayLengthMismatch();}
-        if (length > 1000) {revert ArrayLengthOverMaxLimit();}
-        
+
+        address[] memory failed = new address[](length);
+        uint failedCount;
+
         for (uint i = 0; i < length; i++) {
-            (bool success, ) = recipients[i].call{value: values[i]}("");
-            if (!success) {revert EtherTransferFailed();}
+            (bool success, ) = payable(recipients[i]).call{value: values[i], gas: 2300}("");
+            if (!success) {
+                failed[failedCount] = recipients[i];
+                failedCount++;
+            }
         }
+
+        // Resize the failed array to save space
+        assembly { mstore(failed, failedCount) }
+
+        return failed;
     }
 
     /// @notice Sends tokens to an array of users
